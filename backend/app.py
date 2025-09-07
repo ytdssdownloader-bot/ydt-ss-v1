41a6a011e65b53542d7fb2d9f2b70c5e5c8290d1
Hariharan-Arjunan <hariharanarjunan@gmail.com>
2025-09-07 18:51:20 +0530

    cokkie error

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,21 @@ from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 import yt_dlp
 import io
+import shutil
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+
+
+# Copy secret file to /tmp (writable)
+COOKIES_FILE_READONLY = '/etc/secrets/cookies.txt'
+COOKIES_FILE_WRITABLE = '/tmp/cookies.txt'
+shutil.copy(COOKIES_FILE_READONLY, COOKIES_FILE_WRITABLE)
+
 # COOKIES_FILE = 'cookies.txt'  # Your exported cookies
-COOKIES_FILE = '/etc/secrets/cookies.txt'
+# COOKIES_FILE = '/etc/secrets/cookies.txt'
 
 @app.route('/download', methods=['POST'])
 def download_video():
@@ -17,15 +26,25 @@ def download_video():
     if not url:
         return jsonify({'error': 'URL is required'}), 400
 
+    # ydl_opts = {
+    #     'format': 'best',
+    #     'cookiefile': COOKIES_FILE,
+    #     'noplaylist': True,
+    #     'quiet': True,
+    #     'no_warnings': True,
+    #     'outtmpl': '-',  # Stream output
+    #     'cookiefile_save': False,      # <-- Prevent saving cookies
+    # }
+
     ydl_opts = {
         'format': 'best',
-        'cookiefile': COOKIES_FILE,
+        'cookiefile': COOKIES_FILE_WRITABLE,
         'noplaylist': True,
         'quiet': True,
         'no_warnings': True,
         'outtmpl': '-',  # Stream output
-        'cookiefile_save': False,      # <-- Prevent saving cookies
     }
+
 
     try:
         def generate():
